f2001a94d11a26a38fb30dfa74a5ee2e93c2379b
Tejas <137605358+TejasKarde21@users.noreply.github.com>
2023-07-11 20:02:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#000000",
+        10: '#7d8035',
+        20: '#575920',
+    },
+  },
+  
+  plugins: [],
+}
+}
+
